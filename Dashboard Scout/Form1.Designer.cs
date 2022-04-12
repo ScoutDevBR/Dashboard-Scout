a1ee6378e5b24982a3d2ccda1ffae57b8e927885
aprendiz <aprendiz@DESKTOP-GNBEFGR>
2022-04-12 10:25:20 -0300

    Adicionar arquivos de projeto.

---
+++ Dashboard Scout/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Dashboard_Scout
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnDashboard = new System.Windows.Forms.Button();
+            this.BtnAnalytics = new System.Windows.Forms.Button();
+            this.BtnCalender = new System.Windows.Forms.Button();
+            this.BtnContactUs = new System.Windows.Forms.Button();
+            this.BtnSettings = new System.Windows.Forms.Button();
+            this.PnlNav = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.PnlNav);
+            this.panel1.Controls.Add(this.BtnSettings);
+            this.panel1.Controls.Add(this.BtnContactUs);
+            this.panel1.Controls.Add(this.BtnCalender);
+            this.panel1.Controls.Add(this.BtnAnalytics);
+            this.panel1.Controls.Add(this.BtnDashboard);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(172, 557);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(172, 127);
+            this.panel2.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(60, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(49, 49);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label1.Location = new System.Drawing.Point(41, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "User name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.LightGreen;
+            this.label2.Location = new System.Drawing.Point(28, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Some user text here";
+            // 
+            // BtnDashboard
+            // 
+            this.BtnDashboard.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BtnDashboard.FlatAppearance.BorderSize = 0;
+            this.BtnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnDashboard.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnDashboard.ForeColor = System.Drawing.Color.LimeGreen;
+            this.BtnDashboard.Image = ((System.Drawing.Image)(resources.GetObject("BtnDashboard.Image")));
+            this.BtnDashboard.Location = new System.Drawing.Point(0, 127);
+            this.BtnDashboard.Name = "BtnDashboard";
+            this.BtnDashboard.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.BtnDashboard.Size = new System.Drawing.Size(172, 46);
+            this.BtnDashboard.TabIndex = 2;
+            this.BtnDashboard.Text = "Dashboard";
+            this.BtnDashboard.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.BtnDashboard.UseVisualStyleBackColor = true;
+            this.BtnDashboard.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BtnAnalytics
+            // 
+            this.BtnAnalytics.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BtnAnalytics.FlatAppearance.BorderSize = 0;
+            this.BtnAnalytics.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnAnalytics.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnAnalytics.ForeColor = System.Drawing.Color.LimeGreen;
+            this.BtnAnalytics.Image = ((System.Drawing.Image)(resources.GetObject("BtnAnalytics.Image")));
+            this.BtnAnalytics.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.BtnAnalytics.Location = new System.Drawing.Point(0, 173);
+            this.BtnAnalytics.Name = "BtnAnalytics";
+            this.BtnAnalytics.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.BtnAnalytics.Size = new System.Drawing.Size(172, 46);
+            this.BtnAnalytics.TabIndex = 7;
+            this.BtnAnalytics.Text = "Analytics";
+            this.BtnAnalytics.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.BtnAnalytics.UseVisualStyleBackColor = true;
+            this.BtnAnalytics.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // BtnCalender
+            // 
+            this.BtnCalender.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BtnCalender.FlatAppearance.BorderSize = 0;
+            this.BtnCalender.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnCalender.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnCalender.ForeColor = System.Drawing.Color.LimeGreen;
+            this.BtnCalender.Image = ((System.Drawing.Image)(resources.GetObject("BtnCalender.Image")));
+            this.BtnCalender.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.BtnCalender.Location = new System.Drawing.Point(0, 219);
+            this.BtnCalender.Name = "BtnCalender";
+            this.BtnCalender.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.BtnCalender.Size = new System.Drawing.Size(172, 46);
+            this.BtnCalender.TabIndex = 8;
+            this.BtnCalender.Text = "Calender";
+            this.BtnCalender.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.BtnCalender.UseVisualStyleBackColor = true;
+            // 
+            // BtnContactUs
+            // 
+            this.BtnContactUs.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BtnContactUs.FlatAppearance.BorderSize = 0;
+            this.BtnContactUs.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnContactUs.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnContactUs.ForeColor = System.Drawing.Color.LimeGreen;
+            this.BtnContactUs.Image = ((System.Drawing.Image)(resources.GetObject("BtnContactUs.Image")));
+            this.BtnContactUs.Location = new System.Drawing.Point(0, 265);
+            this.BtnContactUs.Name = "BtnContactUs";
+            this.BtnContactUs.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.BtnContactUs.Size = new System.Drawing.Size(172, 46);
+            this.BtnContactUs.TabIndex = 9;
+            this.BtnContactUs.Text = "Contact Us";
+            this.BtnContactUs.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.BtnContactUs.UseVisualStyleBackColor = true;
+            // 
+            // BtnSettings
+            // 
+            this.BtnSettings.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.BtnSettings.FlatAppearance.BorderSize = 0;
+            this.BtnSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnSettings.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnSettings.ForeColor = System.Drawing.Color.LimeGreen;
+            this.BtnSettings.Image = ((System.Drawing.Image)(resources.GetObject("BtnSettings.Image")));
+            this.BtnSettings.Location = new System.Drawing.Point(0, 511);
+            this.BtnSettings.Name = "BtnSettings";
+            this.BtnSettings.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.BtnSettings.Size = new System.Drawing.Size(172, 46);
+            this.BtnSettings.TabIndex = 10;
+            this.BtnSettings.Text = "Settings";
+            this.BtnSettings.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.BtnSettings.UseVisualStyleBackColor = true;
+            // 
+            // PnlNav
+            // 
+            this.PnlNav.BackColor = System.Drawing.Color.LimeGreen;
+            this.PnlNav.Location = new System.Drawing.Point(0, 169);
+            this.PnlNav.Name = "PnlNav";
+            this.PnlNav.Size = new System.Drawing.Size(3, 100);
+            this.PnlNav.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(901, 557);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Button BtnDashboard;
+        private Label label2;
+        private Button BtnAnalytics;
+        private Button BtnSettings;
+        private Button BtnContactUs;
+        private Button BtnCalender;
+        private Panel PnlNav;
+    }
+}
---
+++ Dashboard Scout/Form1.cs
@@ -0,0 +1,47 @@
+
+
+namespace Dashboard_Scout
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
